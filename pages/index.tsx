c70d25c28963fadc971463b25f3b086b9f2be2a0
igorsheg-wix <igors@wix.com>
2020-03-03 00:39:48 +0200

    wip

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+export default () => {
+    const [state, setstate] = useState<null | string>(null)
+    return (
+        <div>Hello World. </div>
+    )
+}
