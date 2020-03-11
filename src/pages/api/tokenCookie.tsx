f688347cc59106c5f5ed448853112fe9629a2361
igorsheg-wix <igors@wix.com>
2020-03-11 14:05:59 +0200

    Token exp date set to 2 weeks

--- src/pages/api/tokenCookie.tsx
+++ src/pages/api/tokenCookie.tsx
@@ -5,9 +5,12 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     const headers = req.headers.authorization;
     const token = headers?.split("bearer ")[1];
 
+    const exp = new Date();
+    exp.setDate(exp.getDate() + 7 * 2);
+
     const options = {
         path: "/",
-        // expires: new Date(tokens.expiry_date),
+        expires: exp,
         httpOnly: false,
         secure: false
     };
